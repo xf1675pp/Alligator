2578642befd41a3cedf531809c96ed59a78acdbc
xf1675pp <79674314+xf1675pp@users.noreply.github.com>
2021-02-25 19:53:26 -0600

    Initial commit

---
+++ main/java/com/mycompany/moviedrivers/MovieDriver.java
@@ -0,0 +1,56 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.Connection;
+import java.sql.*;
+
+
+/**
+ *
+ * @author Raju
+ */
+public class MovieDriver 
+{
+       public static void main(String[] args)
+       {
+           
+           try {
+               Class.forName("com.mysql.cj.jdbc.Driver");
+               //1.Connecting to database
+               //Connection allConn= DriverManager.getConnection("jdbc:mysql://localhost:3306/omdb");
+        	
+        	   
+        	String url = "jdbc:mysql://localhost:3306/omdb";
+   			 String user = "root";
+   			 String password = "";
+   			Connection allConn = DriverManager.getConnection( url, user, password);
+   			
+               //2. Creating a statement
+               Statement allStmt = allConn.createStatement();
+
+               //3. Sql Query
+               ResultSet allsql = allStmt.executeQuery("SELECT * FROM movies");
+
+               //Process the result
+               while (allsql.next())
+               {
+                   System.out.println(allsql.getString("movie_id"));
+                   System.out.println(allsql.getString("english_name"));
+                   System.out.println(allsql.getString("native_name"));
+                   System.out.println(allsql.getString("year_made"));
+                   System.out.println("------------------------------------------"); 
+               }
+               allsql.close();
+
+       }
+           catch (Exception exc)
+           {
+               exc.printStackTrace();
+           }
+       }
+
+}
