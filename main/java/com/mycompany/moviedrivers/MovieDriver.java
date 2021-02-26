c7191464d718c20f41cad4984ebc7edbf8c7c8c0
tnangsel01 <tenzin.nangsel@my.metrostate.edu>
2021-02-25 20:06:52 -0600

    update deleteMovie

--- main/java/com/mycompany/moviedrivers/MovieDriver.java
+++ main/java/com/mycompany/moviedrivers/MovieDriver.java
@@ -15,6 +15,13 @@ import java.sql.*;
  */
 public class MovieDriver 
 {
+	/* this method will delete the movie that has same movie_id input. 
+	 * return true if movie with the movie_id has been deleted.
+	 * return false if movie with the movie_id has fail to delete.
+	 */
+	public boolean deleteMovie(int movie_id){
+		
+	}
        public static void main(String[] args)
        {
            
